0f548eb9a126f9dc9c79c21888b1f11ccaaffb65
lirixiang <565539277@qq.com>
2020-03-30 15:25:51 +0800

    sql

--- operbench/config/secure.py
+++ operbench/config/secure.py
@@ -12,10 +12,13 @@ ENV = "development"
 # DEBUG在Flask中有一个默认值为False
 
 # DB配置
-import os
-basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+# uri统一资源匹配符
+# SQLALCHEMY_DATABASE_URI配置数据库连接的参数
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123456@127.0.0.1:3306/school?charset=utf8'
+# 请求结束后自动提交数据库修改
+SQLALCHEMY_COMMMIT_ON_TEARDOWN = True
+# 如果设置成 True (默认情况)，Flask-SQLAlchemy	将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它。
+
 
 SECRET_KEY = "this is a secret key"
 

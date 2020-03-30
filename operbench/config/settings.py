8d5c5fe8bc414107d33323ea7c09de3ec7de8d5e
lirixiang <565539277@qq.com>
2020-03-30 18:43:00 +0800

    logs

--- operbench/config/settings.py
+++ operbench/config/settings.py
@@ -12,12 +12,12 @@ from logging.handlers import RotatingFileHandler
 # 启动的监听端口和IP也以在这里配置
 # 启动的监听端口和IP也以在这里配置
 # 日志配置
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,)
 # 创建日志记录器，日志保存到哪里，日志文件大小....
 import os
 if not os.path.isdir("./logs"):
     os.makedirs("logs")
-file_log_handler = RotatingFileHandler("logs/log.log", maxBytes=1024, backupCount=5)
+file_log_handler = RotatingFileHandler("logs/log.log", maxBytes=1024, backupCount=5,encoding="utf8")
 # 创建日志格式
 formatter = logging.Formatter("%(levelname)s %(filename)s: %(lineno)d %(message)s")
 file_log_handler.setFormatter(formatter)
